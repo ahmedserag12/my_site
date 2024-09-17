4714563a91eb85f787919b6c30d8946b4538e3ac
ahmed serag <seragahmad85@gmail.com>
2024-09-17 21:50:22 +0300

    ading databse models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post, Tag, Author
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'tags', 'date')
+    list_display = ('title', 'date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag)
+admin.site.register(Author)
